8fb07780fb86a3334b11c4d9d39155d082d89afd
Calvin Baker <calvin.baker128@gmail.com>
2019-12-07 22:01:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,3 +12,23 @@ end
 def map_to_no_change(arr)
   return arr
 end
+
+def map_to_double(arr)
+  i = 0
+  new_arr = []
+  while i < arr.length do
+    new_arr.push(arr[i] * 2)
+    i += 1
+  end
+  new_arr
+end
+
+def map_to_square(arr)
+  i = 0
+  new_arr = []
+  while i < arr.length do
+    new_arr.push(arr[i] * -1)
+    i += 1
+  end
+  new_arr
+end
