3933f4f76b0a0363bf09ae2ce80c193645e01146
Calvin Baker <calvin.baker128@gmail.com>
2019-12-07 22:10:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -42,3 +42,14 @@ def reduce_to_total(arr, start = 0)
   end
   total
 end
+
+def reduce_to_all_true(arr)
+  i = 0 
+  while i < arr.length do
+    if arr[i] == false
+      return false
+    end
+    i += 1
+  end
+  return true
+end
